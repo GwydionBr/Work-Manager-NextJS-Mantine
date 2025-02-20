fb36a46fcd9106f4648d37e550e5bf1947ae53f5
GwydionBr <gwydie@googlemail.com>
2025-02-20 14:09:43 +0100

    upadte TimeTrackerComponent positioning to fixed

--- src/app/work/layout.tsx
+++ src/app/work/layout.tsx
@@ -5,7 +5,7 @@ import * as actions from '@/actions';
 import ProjectNavbar from '@/components/Navbar/ProjectNavbar';
 import type { Tables } from '@/db.types';
 import TimeTrackerComponent from '@/components/TimeTracker/TimeTrackerComponent';
-import { Grid } from '@mantine/core';
+import { Grid, Flex } from '@mantine/core';
 
 export default function WorkLayout({ children }: { children: any }) {
   const [projects, setProjects] = useState<Tables<'timerProject'>[]>([]);
@@ -29,7 +29,9 @@ export default function WorkLayout({ children }: { children: any }) {
         {children}
       </Grid.Col>
       <Grid.Col span={3}>
-        <TimeTrackerComponent />
+        <Flex direction='column' justify='center' style={{ height: '100vh', position: 'fixed', right: 0 }}>
+          <TimeTrackerComponent />
+        </Flex>
       </Grid.Col>
     </Grid>
   );
