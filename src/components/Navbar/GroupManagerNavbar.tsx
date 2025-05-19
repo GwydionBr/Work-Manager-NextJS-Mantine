c978c11d763f6da7b4e2937836065c2591b2b7b2
GwydionBr <gwydie@googlemail.com>
2025-05-19 16:01:37 +0200

    Refactor FinanceNavbar component: remove unused imports and filter logic for cash flows, simplifying the structure and enhancing maintainability.

--- src/components/Navbar/GroupManagerNavbar.tsx
+++ src/components/Navbar/GroupManagerNavbar.tsx
@@ -1,19 +1,9 @@
 "use client";
 
-import { Group, ScrollArea, Stack, Text, Divider, Box } from "@mantine/core";
+import { Group, ScrollArea, Stack, Text, Box } from "@mantine/core";
 import classes from "./Navbar.module.css";
-import FinanceSection from "../Finances/FinanceSection";
-import { useFinanceStore } from "@/stores/financeStore";
 
 export default function FinanceNavbar() {
-  const { singleCashFlows } = useFinanceStore();
-
-  const incomeCashFlows = singleCashFlows.filter(
-    (cashFlow) => cashFlow.type === "income"
-  );
-  const expenseCashFlows = singleCashFlows.filter(
-    (cashFlow) => cashFlow.type === "expense"
-  );
 
   return (
     <Box className={classes.main} w="250px">
@@ -23,9 +13,6 @@ export default function FinanceNavbar() {
 
       <ScrollArea className={classes.scrollArea}>
         <Stack className={classes.financeSections} gap={0}>
-          <FinanceSection title="Income" cashFlows={incomeCashFlows} />
-          <Divider className={classes.divider} />
-          <FinanceSection title="Expenses" cashFlows={expenseCashFlows} />
         </Stack>
       </ScrollArea>
     </Box>
