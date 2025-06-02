9e0aadb6335ca4115c4a798ba7c7721b106e2b7a
GwydionBr <gwydie@googlemail.com>
2025-06-02 13:06:29 +0200

    Add logging for production environment check: Include a console log statement to output the current production status in the RootLayout component, aiding in debugging and environment verification.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,6 +19,7 @@ export const metadata = {
 
 export default function RootLayout({ children }: { children: any }) {
   const isProduction = process.env.NODE_ENV === "production";
+  console.log("isProduction", isProduction);
 
   return (
     <html lang="en" {...mantineHtmlProps}>
