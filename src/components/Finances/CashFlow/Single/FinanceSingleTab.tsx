ca69490a000bbaf3dae5506a5f93ea27cbd3e13e
GwydionBr <gwydie@googlemail.com>
2025-09-22 13:56:31 +0200

    Enhance FinanceSingleTab and SingleCashflowRow components with delete functionality and improved user interaction
    - Integrated delete confirmation modal in FinanceSingleTab for managing single cash flows.
    - Updated SingleCashflowRow to include edit and delete actions via a dropdown menu, enhancing user experience.
    - Refactored state management in FinanceSingleTab to ensure selected cash flow is set based on sorted data.
    - Improved layout and styling in SingleCashflowRow for better visual consistency and interaction.

--- src/components/Finances/CashFlow/Single/FinanceSingleTab.tsx
+++ src/components/Finances/CashFlow/Single/FinanceSingleTab.tsx
@@ -1,12 +1,11 @@
 "use client";
 
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useDisclosure } from "@mantine/hooks";
 import { useFinanceStore } from "@/stores/financeStore";
 import { useSettingsStore } from "@/stores/settingsStore";
 
 import {
-  Table,
   Group,
   Text,
   ScrollArea,
@@ -17,23 +16,22 @@ import {
 } from "@mantine/core";
 import EditCashFlowDrawer from "@/components/Finances/CashFlow/EditCashFlowDrawer";
 import CashFlowModal from "@/components/Finances/CashFlow/CashFlowModal";
-import EditActionIcon from "@/components/UI/ActionIcons/EditActionIcon";
 
-import { formatDate, formatMoney } from "@/utils/formatFunctions";
+import { formatDate } from "@/utils/formatFunctions";
 import { Tables } from "@/types/db.types";
-import {
-  IconCashMove,
-  IconCashMoveBack,
-  IconCashPlus,
-  IconList,
-  IconTag,
-} from "@tabler/icons-react";
+import { IconCashPlus } from "@tabler/icons-react";
 import DelayedTooltip from "@/components/UI/DelayedTooltip";
 import SingleCashflowRow from "./SingleCashflowRow";
 import FinancesNavbar from "../../FinancesNavbar";
+import { modals } from "@mantine/modals";
+import {
+  showActionErrorNotification,
+  showActionSuccessNotification,
+  showDeleteConfirmationModal,
+} from "@/utils/notificationFunctions";
 
 export default function FinanceSingleTab() {
-  const { singleCashFlows, financeCategories } = useFinanceStore();
+  const { singleCashFlows, deleteSingleCashFlow } = useFinanceStore();
   const { locale } = useSettingsStore();
   const [
     editCashFlowOpened,
@@ -44,10 +42,44 @@ export default function FinanceSingleTab() {
     { open: openCashFlowModal, close: closeCashFlowModal },
   ] = useDisclosure(false);
   const [selectedCashFlow, setSelectedCashFlow] =
-    useState<Tables<"single_cash_flow"> | null>(singleCashFlows[0] ?? null);
+    useState<Tables<"single_cash_flow"> | null>(null);
   const sortedSingleCashFlows = singleCashFlows.sort(
     (a, b) => new Date(b.date).getTime() - new Date(a.date).getTime()
   );
+
+  useEffect(() => {
+    if (sortedSingleCashFlows.length > 0) {
+      setSelectedCashFlow(sortedSingleCashFlows[0]);
+    }
+  }, [sortedSingleCashFlows]);
+
+  const handleDelete = (id: string) => {
+    showDeleteConfirmationModal(
+      locale === "de-DE" ? "Einmalzahlung löschen" : "Delete Single Cash Flow",
+      locale === "de-DE"
+        ? "Sind Sie sicher, dass Sie diese Einmalzahlung löschen möchten?"
+        : "Are you sure you want to delete this single cash flow?",
+      async () => {
+        const deleted = await deleteSingleCashFlow(id);
+        if (deleted) {
+          showActionSuccessNotification(
+            locale === "de-DE"
+              ? "Einmalzahlung erfolgreich gelöscht"
+              : "Single cash flow deleted successfully",
+            locale
+          );
+        } else {
+          showActionErrorNotification(
+            locale === "de-DE"
+              ? "Einmalzahlung konnte nicht gelöscht werden"
+              : "Single cash flow could not be deleted",
+            locale
+          );
+        }
+      },
+      locale
+    );
+  };
 
   return (
     <Group>
@@ -95,7 +127,15 @@ export default function FinanceSingleTab() {
                     labelPosition="left"
                   />
                 )}
-                <SingleCashflowRow cashflow={cashFlow} ml="xl" />
+                <SingleCashflowRow
+                  cashflow={cashFlow}
+                  ml="xl"
+                  onEdit={() => {
+                    setSelectedCashFlow(cashFlow);
+                    openEditCashFlow();
+                  }}
+                  onDelete={() => handleDelete(cashFlow.id)}
+                />
               </Stack>
             );
           })}
--- src/components/Finances/CashFlow/Single/SingleCashflowRow.tsx
+++ src/components/Finances/CashFlow/Single/SingleCashflowRow.tsx
@@ -1,24 +1,49 @@
 "use client";
 
+import { useDisclosure, useHover } from "@mantine/hooks";
 import { useFinanceStore } from "@/stores/financeStore";
 import { useSettingsStore } from "@/stores/settingsStore";
 
-import { Badge, Card, CardProps, Grid, Text, Group } from "@mantine/core";
-import { IconCashMoveBack, IconCashMove, IconTag } from "@tabler/icons-react";
+import {
+  Badge,
+  Card,
+  CardProps,
+  Grid,
+  Text,
+  Group,
+  Menu,
+  Transition,
+} from "@mantine/core";
+import {
+  IconCashMoveBack,
+  IconCashMove,
+  IconTag,
+  IconPencil,
+  IconTrash,
+} from "@tabler/icons-react";
 
-import { formatDate, formatMoney } from "@/utils/formatFunctions";
+import { formatMoney } from "@/utils/formatFunctions";
 import { Tables } from "@/types/db.types";
+import MoreActionIcon from "@/components/UI/ActionIcons/MoreActionIcon";
+
+interface SingleCashflowRowProps extends CardProps {
+  cashflow: Tables<"single_cash_flow">;
+  onEdit: () => void;
+  onDelete: () => void;
+}
 
 export default function SingleCashflowRow({
   cashflow,
+  onEdit,
+  onDelete,
   ...props
-}: {
-  cashflow: Tables<"single_cash_flow">;
-} & CardProps) {
+}: SingleCashflowRowProps) {
   const { locale } = useSettingsStore();
   const { financeCategories } = useFinanceStore();
+  const [opened, { open, close }] = useDisclosure(false);
+  const { hovered, ref } = useHover();
   return (
-    <Card withBorder shadow="sm" radius="md" p="sm" {...props}>
+    <Card withBorder shadow="sm" h={45} radius="md" p="xs" {...props} ref={ref}>
       <Grid>
         <Grid.Col span={2}>
           <Group>
@@ -32,21 +57,52 @@ export default function SingleCashflowRow({
             </Text>
           </Group>
         </Grid.Col>
-        <Grid.Col span={7}>
+        <Grid.Col span={6}>
           <Text>{cashflow.title}</Text>
         </Grid.Col>
         <Grid.Col span={3}>
-          <Badge
-            color="grape"
-            variant="light"
-            leftSection={<IconTag size={12} />}
+          {cashflow.category_id && (
+            <Badge
+              color="grape"
+              variant="light"
+              leftSection={<IconTag size={12} />}
+            >
+              {
+                financeCategories.find(
+                  (category) => category.id === cashflow.category_id
+                )?.title
+              }
+            </Badge>
+          )}
+        </Grid.Col>
+        <Grid.Col span={1}>
+          <Transition
+            mounted={hovered || opened}
+            transition="fade-left"
+            duration={200}
           >
-            {
-              financeCategories.find(
-                (category) => category.id === cashflow.category_id
-              )?.title
-            }
-          </Badge>
+            {(styles) => (
+              <Menu opened={opened} onClose={close}>
+                <Menu.Target>
+                  <MoreActionIcon onClick={open} style={styles} />
+                </Menu.Target>
+                <Menu.Dropdown>
+                  <Menu.Item
+                    onClick={onEdit}
+                    leftSection={<IconPencil size={16} />}
+                  >
+                    {locale === "de-DE" ? "Bearbeiten" : "Edit"}
+                  </Menu.Item>
+                  <Menu.Item
+                    onClick={onDelete}
+                    leftSection={<IconTrash size={16} />}
+                  >
+                    {locale === "de-DE" ? "Löschen" : "Delete"}
+                  </Menu.Item>
+                </Menu.Dropdown>
+              </Menu>
+            )}
+          </Transition>
         </Grid.Col>
       </Grid>
     </Card>
