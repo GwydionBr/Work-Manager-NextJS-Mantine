6014e0eb0f13d57c6526f9bf54a364bc81acff49
GwydionBr <gwydie@googlemail.com>
2025-12-02 14:02:10 +0100

    Update import path for getAllBankAccounts function to reflect new file structure

--- src/utils/queries/finances/use-bank-account.ts
+++ src/utils/queries/finances/use-bank-account.ts
@@ -1,7 +1,7 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { getAllBankAccounts } from "@/actions/finance/bankAccount/getAllBankAccounts";
+import { getAllBankAccounts } from "@/actions/finance/bankAccount/get-all-bank-accounts";
 
 // Query to get all bank accounts
 export function useBankAccountQuery() {
